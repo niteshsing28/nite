5248ba84dfb94ff441f07795a2a9dc43d7847e55
nitesh <nitesh28@gmail.com>
2023-06-22 11:26:16 +0530

    i need 50

--- login.java
+++ login.java
@@ -3,7 +3,7 @@ public class Login
     public static void main(string args[])
             int i;
 
-    for (i=1;i<=10;i++){
+    for (i=1;i<=50;i++){
     System.out.println("number: " +i);
     }
 }
